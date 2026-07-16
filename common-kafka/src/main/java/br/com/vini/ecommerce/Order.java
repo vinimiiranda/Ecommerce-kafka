3cf62ac2c4b664f81c246c8b73e3b5838a484870
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-07-15 23:13:25 -0300

    feat: adiciona serviço de usuários e centraliza contrato Order

---
+++ common-kafka/src/main/java/br/com/vini/ecommerce/Order.java
@@ -0,0 +1,34 @@
+package br.com.vini.ecommerce;
+
+import java.math.BigDecimal;
+
+public class Order {
+
+
+    private final String userId, orderId;
+    private final BigDecimal amount;
+
+    public Order(String userId, String orderId, BigDecimal value) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.amount = value;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "userId='" + userId + '\'' +
+                ", orderId='" + orderId + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
