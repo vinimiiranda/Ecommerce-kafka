17acb9e500acb34134dfc02275e0fb65bcbe5e7e
Vinicius de Miranda melo <161662601+vinimiiranda@users.noreply.github.com>
2026-09-16 15:47:57 -0300

    fix: corrige topico do evento de email para ECOMMERCE_SEND_EMAIL

--- service-new-order/src/main/java/br/com/vini/ecommerce/NewOrderMain.java
+++ service-new-order/src/main/java/br/com/vini/ecommerce/NewOrderMain.java
@@ -18,7 +18,7 @@ public class NewOrderMain {
                     orderdispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
 
                     var emailCode = "Thank you for order! We are processing your order!";
-                    emaildispatcher.send("ECOMMERCE_NEW_EMAIL", email, emailCode);
+                    emaildispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailCode);
 
                 }
 
